d512f8535d17b00277f13aa988a9f27074ad78f2
lucasheller <lucas.heller@hotmail.com>
2020-08-25 20:53:32 -0300

    INITIAL COMMIT

---
+++ src/test/java/com/notification/lov/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notification.lov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
