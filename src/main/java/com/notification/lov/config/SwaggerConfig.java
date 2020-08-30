2f7892a927a071e0a1fd4063485dee86e067e32f
Lucas Heller Papa Crepaldi Conceicao <lcrepaldi@uolinc.com>
2020-08-29 22:45:57 -0300

    ADD Swagger config class

---
+++ src/main/java/com/notification/lov/config/SwaggerConfig.java
@@ -0,0 +1,30 @@
+package com.notification.lov.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.notification.lov.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(
+                        new ApiInfoBuilder()
+                                .title("Notification API")
+                                .description("Notificate lov users")
+                                .build());
+    }
+
+}
